2fa04d76659154b7fac44afbfe4e92ba61bec305
xrr2016 <xiaoranran1993@163.com>
2017-09-12 22:49:26 +0800

    :art:

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
-var ws = require("nodejs-websocket")
+const ws = require("nodejs-websocket")
 const port = 8000
-// Scream server example: "hi" -> "HI!!!"
-var server = ws.createServer(function (conn) {
-	console.log("New connection")
-	conn.on("text", function (str) {
-		console.log("Received " + str)
-		conn.sendText(str.toUpperCase()+"!!!")
+
+let clientCount = 0
+
+const broadcast = function (server, msg) {
+	server.connections.forEach(function(connection) {
+		connection.sendText(msg)
 	})
+}
+
+const server = ws.createServer(function (conn) {
+	clientCount++
+	conn.nickname = 'user' + clientCount
+	broadcast(server, `${conn.nickname} comes in.`)
+  console.log(clientCount)
+	conn.on("text", function (msg) {
+	  broadcast(server, msg)
+	})
+
 	conn.on("close", function (code, reason) {
-		console.log("Connection closed")
+	  broadcast(server, `${conn.nickname} left.`)
 	})
 	conn.on("error", function (err) {
 		console.log("Error", err)
