ce881cd4c6375d65be493e4f902fd1cd2beaec17
xrr2016 <xiaoranran1993@163.com>
2017-09-26 08:36:20 +0800

    :fire:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/takeWhile';
+import 'rxjs/add/operator/do';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  max     = 1
+  current = 0
+
+  start ():void {
+    const interval = Observable.interval(100)
+
+    interval
+      .takeWhile(_ => !this.isFinished)
+      .do(i => this.current += 0.1)
+      .subscribe()
+  }
+
+  finish () {
+    this.current = this.max
+  }
+
+  reset () {
+    this.current = 0
+  }
+
+  get maxVal () {
+    return isNaN(this.max) || this.max < 0.1 ? 0.1 : this.max
+  }
+
+  get currentVal () {
+    return isNaN(this.current) || this.current < 0 ? 0 : this.current
+  }
+
+  get isFinished () {
+    return this.currentVal >= this.maxVal
+  }
+
 }
+
+
+
+
+
+
+
+
+
